b86184ae8e026254dd812cba45dc6b77c4346e0a
Mario Lang <e11901887@student.tuwien.ac.at>
2023-06-07 11:49:13 +0200

    updated the filepath to be relative to the bin directory

--- test/ClassDecoder.cpp
+++ test/ClassDecoder.cpp
@@ -5,7 +5,7 @@ int main(){
 
     auto dec = Decoder();
 
-    cv::Mat image = cv::imread("/mnt/c/Users/mario/TU/TU_Semester_8_OTTO/Prog_CPP/GroupNProject/QR_Code_Foto_Technopedia.jpg");
+    cv::Mat image = cv::imread("../../QR_Code_Foto_Technopedia.jpg");
     int err = dec.decode(image);
 
     return err;
