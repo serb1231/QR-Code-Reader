675447ea6574270267fe46b9fcea9d371c04ec3f
Matteo Ingegneri <e11917678@student.tuwien.ac.at>
2023-06-09 11:08:32 +0200

    tests using zxing

---
+++ include/ZXDecoder.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+
+/// This class contains all the functionality to actually encode a QR-Code
+
+class Encoder{
+    public:
+
+    int encode();
+};
