6d372820c1009533466d87e01e633d8332e88784
Mario Lang <e11901887@student.tuwien.ac.at>
2023-06-13 12:37:45 +0200

    changed Decoder to return std::string data instead of int

--- lib/Decoder.cpp
+++ lib/Decoder.cpp
@@ -1,12 +1,12 @@
 #include <Decoder.hpp>
 #include <opencv2/opencv.hpp>
 
-int Decoder::decode(cv::Mat QRimage){
+std::string Decoder::decode(cv::Mat QRimage){
 
     /// Check if the image was successfully loaded
     if (QRimage.empty()) {
         std::cout << "Failed to load the image." << std::endl;
-        return -1;
+        return "No Image";
     }
 
     /// Check if image is in size range
@@ -16,7 +16,7 @@ int Decoder::decode(cv::Mat QRimage){
     /// Too small images can't be decoded
     if(width < 199 || height < 199){
         std::cout << "Image too small, please upload a bigger image." << std::endl;
-        return -1;
+        return "Image too small";
     }
 
     /// crop Image if code is detected
@@ -40,13 +40,6 @@ int Decoder::decode(cv::Mat QRimage){
         /// Crop the image using the ROI
         croppedImage = QRimage(roi);
 
-        /// Display the cropped image
-        cv::imshow("Cropped Image", croppedImage);
-
-        /// Display the image with the detected QR code
-        cv::imshow("Detected QR Code", QRimage);
-        cv::waitKey(0);
-
     } else {
         std::cout << "No QR code detected!" << std::endl;
     }
@@ -63,8 +56,6 @@ int Decoder::decode(cv::Mat QRimage){
         cv::resize(croppedImage, croppedImage, cv::Size(width, height));
     }
 
-    std::cout << "Image successfully resized." << std::endl;
-
     /// Convert image to binary image
     cv::Mat binaryImage;
     cv::threshold(croppedImage, binaryImage, 128, 255, cv::THRESH_BINARY);
@@ -75,19 +66,13 @@ int Decoder::decode(cv::Mat QRimage){
     if (!data.empty())
     {
         /// QR code detected and decoded successfully
-        std::cout << "Decoded data: " << data << std::endl;
-
-        /// Display the result
-        cv::drawContours(binaryImage, std::vector<std::vector<cv::Point>>{points}, -1, cv::Scalar(0, 255, 0), 2);
-        cv::imshow("QR Code", binaryImage);
-        cv::waitKey(0);
-        return 0;
+        return data;
     }
     else
     {
         /// QR code not detected or decoding failed
         std::cout << "QR code not found or decoding failed" << std::endl;
-        return -1;
+        return "No QR-Code";
     }
 
 }
